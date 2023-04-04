303013cd72ce7615ee0b1daf9249b8577e70ee1a
somin18501 <somingandhi@gmail.com>
2023-04-04 09:43:03 +0530

    till login

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,23 @@ import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
 import Layout from './Layout'
 import RegisterPage from './pages/RegisterPage'
+import axios from 'axios'
+import { UserContextProvider } from './UserContext'
+
+axios.defaults.baseURL = 'http://127.0.0.1:4000';
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<IndexPage/>} />
-        <Route path='/login' element={<LoginPage/>} />
-        <Route path='/register' element={<RegisterPage/>} />
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<IndexPage/>} />
+          <Route path='/login' element={<LoginPage/>} />
+          <Route path='/register' element={<RegisterPage/>} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   )
 }
 
