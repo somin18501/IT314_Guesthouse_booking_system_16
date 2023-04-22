a85d1dd74afee4f928c4712bce8b414a285a0ef2
Harshpatel2910 <77285337+Harshpatel2910@users.noreply.github.com>
2023-04-22 11:14:50 +0530

    Update auth.test.js

--- api/__test__/auth.test.js
+++ api/__test__/auth.test.js
@@ -1,8 +1,18 @@
+const app = require("../index");
+const request = require("supertest");
+
 describe("GET /", () => {
   test("should return 'test ok'", async () => {
     const response = await request(app).get("/");
     expect(response.status).toBe(200);
     
     
+  });
+});
+describe("GET /profile", () => {
+  test("should return null without a valid token", async () => {
+    const response = await request(app).get("/profile");
+    expect(response.status).toBe(200);
+    expect(response.body).toBeNull();
   });
 });
