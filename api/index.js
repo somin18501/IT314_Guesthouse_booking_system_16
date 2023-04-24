2e7c78c3e62cf9329d00e93f4fc93800072632ad
somin18501 <77292724+somin18501@users.noreply.github.com>
2023-04-24 19:17:25 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,12 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://frontend-sar7.onrender.com",
+  })
+);
 
 
 
