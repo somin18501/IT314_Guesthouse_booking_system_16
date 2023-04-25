2286cecf110e2d7360f56a841b11c2d2da6e3226
somin18501 <77292724+somin18501@users.noreply.github.com>
2023-04-26 00:53:29 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -180,7 +180,7 @@ app.post("/login",async (req, res) => {
         {},
         (err, token) => {
           if (err) throw err;
-          res.cookie("token", token, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true});
+          res.cookie("token", token);
         }
 
       );
