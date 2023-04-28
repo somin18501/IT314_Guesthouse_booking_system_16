a86b15dd396b7fa08835d4699c2bf75644e6937f
Harshpatel2910 <hppatel530@gmail.com>
2023-04-28 14:12:23 +0530

    added env variable in index.js

--- api/index.js
+++ api/index.js
@@ -122,7 +122,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     credentials: true,
-    origin: "http://127.0.0.1:5173",
+    origin: process.env.FRONTEND_URL,
+    
   })
 );
 
